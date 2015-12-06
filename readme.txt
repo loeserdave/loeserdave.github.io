3914b2ee54de6b337163b6e5f54a8b9762e286ef
loeserdave <d-loeser@kellogg.northwestern.edu>
2015-12-05 19:24:11 -0600

    final project attempt 1

---
+++ readme.txt
@@ -0,0 +1,1 @@
+www.daveloeser.me
---
+++ todolist.js
@@ -0,0 +1,17 @@
+// This code runs when the page loads
+$(function() {
+
+  $("#add-record").on("click", function(event) {
+    // Do something
+    // on function takes behaviour then function
+    event.preventDefault();
+    $('li').show()
+  });
+
+  $("#remove-record").on("click", function(event) {
+    event.preventDefault();
+    var elementThatWasClicked = $(this);
+    elementThatWasClicked.parent().hide();
+  })
+
+})
